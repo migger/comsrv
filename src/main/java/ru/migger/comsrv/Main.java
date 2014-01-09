685eeae0153a6cbaf2be788dddd92c46cfdfd3b4
migger <migger@yandex.ru>
2014-01-09 23:02:42 +0400

    Initial commit

---
+++ src/main/java/ru/migger/comsrv/Main.java
@@ -0,0 +1,12 @@
+package ru.migger.comsrv;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    public static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] strings) throws Exception {
+
+    }
+}
---
+++ src/test/java/ru/migger/comsrv/MainTest.java
@@ -0,0 +1,19 @@
+package ru.migger.comsrv;
+
+import org.junit.Test;
+
+public class MainTest {
+    public static final int PORT = 8080;
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+        Main.main(new String[0]);
+
+    }
+
+    @Test
+    public void testWorks() throws Exception {
+
+
+    }
+}
